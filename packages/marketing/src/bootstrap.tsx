033edcf07358a67a2cb8857d14c67751fec9920f
Zeztron <pateltharsh@gmail.com>
2021-04-14 09:44:27 -0400

    assembled the app component for marketing and added ts support

---
+++ packages/marketing/src/bootstrap.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mount = (element: HTMLElement) => {
+  ReactDOM.render(<App />, element);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.getElementById('_marketing-dev-root');
+
+  if (devRoot) mount(devRoot);
+}
+
+export { mount };
