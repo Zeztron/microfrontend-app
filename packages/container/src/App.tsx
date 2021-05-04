bb8f0ac8dbd7c809b27170fb019bdf97085d44e8
Zeztron <pateltharsh@gmail.com>
2021-05-04 09:33:01 -0400

    fixing class name collisions

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 const App: React.FC = () => {
   const onSignOut = () => {};
 
   return (
     <BrowserRouter>
-      <div>
-        <Header signedIn={false} onSignOut={onSignOut} />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header signedIn={false} onSignOut={onSignOut} />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
