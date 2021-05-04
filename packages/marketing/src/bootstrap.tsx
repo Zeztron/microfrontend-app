4916e3ab2a5fe9b4c676eb209e3f315029368e4a
Zeztron <pateltharsh@gmail.com>
2021-05-04 17:42:35 -0400

    added memory router

--- packages/marketing/src/bootstrap.tsx
+++ packages/marketing/src/bootstrap.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
 const mount = (element: HTMLElement) => {
-  ReactDOM.render(<App />, element);
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history} />, element);
 };
 
 if (process.env.NODE_ENV === 'development') {
